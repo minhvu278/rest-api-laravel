d96e6344c679c47e018b13559493f7a3163162f8
vudo <dominhvu278@gmail.com>
2022-09-12 22:42:17 +0700

    add model image and album

---
+++ database/migrations/2022_09_11_180400_create_albums_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Models\Album;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('path', 2000);
+            $table->string('type', 25);
+            $table->string('output_path', 2000);
+            $table->text('data');
+            $table->timestamps();
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->foreignIdFor(Album::class, 'album_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
---
+++ database/migrations/2022_09_12_153535_create_image_manipulations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateImageManipulationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('image_manipulations', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+            $table->foreignIdFor(User::class, 'user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('image_manipulations');
+    }
+}
